8de1b59be0c5271401aa902062af46fd48322b6a
David <deh0_@hotmail.com>
2020-08-03 21:57:32 -0300

    Criado rota para tela de cadastro do cliente, e as funções para realizar as operações de CRUD

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('loja', ['ngRoute', 'ngResource','ngAnimate']);
 
-app.config(function($routeProvider, $provide, $httpProvider, $locationProvider) {
+app.config(function($routeProvider) {
 
 	$routeProvider.when("/clientelist", {
 				controller : "clienteController",
@@ -22,7 +22,74 @@ app.config(function($routeProvider, $provide, $httpProvider, $locationProvider) 
 			});
 });
 
+app.controller('clienteController', function($scope, $http, $location, $routeParams) {
 
+	if($routeParams.id != null && $routeParams.id != undefined
+			&& $routeParams.id != ''){
+		
+	$http.get("cliente/buscarcliente/" + $routeParams.id).success(function(response) {
+			$scope.cliente = response;
+			
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+		
+	}else {
+		$scope.cliente = {};
+	}
 
+	$scope.editarCliente = function(id){
+		$location.path('clienteedit/' + id);
+	};
+	
+	$scope.salvarCliente = function () {
+		$http.post("cliente/salvar", $scope.cliente).success(function(response) {
+			$scope.cliente = {};
+			sucesso("Gravado com sucesso!");
+			
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
 
+	$scope.listarClientes = function(){
+		$http.get("cliente/listar").success(function(response) {
+			$scope.data = response;
+
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
+
+	$scope.removerCliente = function(codCliente) {
+		$http.delete("cliente/deletar/" + codCliente).success(function(response) {
+			$scope.listarClientes();
+			sucesso("Removido com sucesso!");
+			
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+	};
+	
+});
+
+function sucesso(msg) {
+	$.notify({
+    	message: msg
+
+    },{
+    	type: 'success',
+        timer: 1000
+    });
+}
+
+function erro(msg) {
+	$.notify({
+    	message: msg
+
+    },{
+    	type: 'danger',
+        timer: 1000
+    });
+}
 
