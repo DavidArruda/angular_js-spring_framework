f25a19e364c64d1479de9c3ce28d8251fdf1cffd
David <deh0_@hotmail.com>
2020-09-09 18:49:32 -0300

    Criação do controllerAngular para fornecedor

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -17,10 +17,145 @@ app.config(function($routeProvider) {
 				templateUrl : "cliente/cadastro.html"
 			})
 			
+			
+			
+			
+			$routeProvider.when("/fornecedorlist", {
+				controller : "fornecedorController",
+				templateUrl : "fornecedor/list.html"
+			})
+			
+			.when("/fornecedoredit/:id", {
+				controller : "fornecedorController",
+				templateUrl : "fornecedor/cadastro.html"
+			})
+			
+			.when("/fornecedor/cadastro", {
+				controller : "fornecedorController",
+				templateUrl : "fornecedor/cadastro.html"
+			})
+			
 			.otherwise({
 				redirectTo : "/"
 			});
 });
+
+
+
+app.controller('fornecedorController', function($scope, $http, $location, $routeParams) {
+
+if($routeParams.id != null && $routeParams.id != undefined
+			&& $routeParams.id != ''){
+		
+	$http.get("fornecedor/buscarFornecedor/" + $routeParams.id).success(function(response) {
+			$scope.fornecedor = response;
+			
+			document.getElementById("imagemFornecedor").src = $scope.fornecedor.foto;
+			
+			setTimeout(function () {
+				$("#selectEstados").prop('selectedIndex', (new Number($scope.fornecedor.estados.id) + 1));
+				
+				$http.get("cidades/listar/" + $scope.fornecedor.estados.id).success(function(response) {
+					$scope.cidades = response;
+					setTimeout(function () {
+						$("#selectCidades").prop('selectedIndex', buscarKeyJson(response, 'id', $scope.fornecedor.cidades.id));
+					}, 1000);
+					
+				}).error(function(data, status, headers, config) {
+					erro("Error: " + status);
+				});
+			
+			}, 1000);
+			
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+		
+	}else {
+		$scope.fornecedor = {};
+	}
+
+	$scope.editarFornecedor = function(id){
+		$location.path('fornecedoredit/' + id);
+	};
+	
+	$scope.salvarFornecedor = function () {
+		$scope.fornecedor.foto = document.getElementById("imagemFornecedor").getAttribute("src");
+	
+		$http.post("fornecedor/salvar", $scope.fornecedor).success(function(response) {
+			$scope.fornecedor = {};
+			document.getElementById("imagemFornecedor").src = '';
+			sucesso("Gravado com sucesso!");
+			
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
+
+	$scope.listarFornecedor = function(numeroPagina){
+		$scope.numeroPagina = numeroPagina;
+		$http.get("fornecedor/listar/" + numeroPagina).success(function(response) {
+			$scope.data = response;
+			
+			$http.get("fornecedor/totalPagina").success(function(response) {
+				$scope.totalPagina = response;
+			}).error(function(response) {
+				erro("Error: " + response);
+			});
+
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
+
+	$scope.proximo = function(){
+		if(new Number($scope.numeroPagina) < new Number($scope.totalPagina)){
+			$scope.listarFornecedor(new Number($scope.numeroPagina + 1));
+		}
+		
+	};
+	
+	$scope.anterior = function(){
+		if(new Number($scope.numeroPagina) > 1) {
+			$scope.listarFonecedores(new Number($scope.numeroPagina - 1));
+		}
+	};
+
+	$scope.removerFornecedor = function(codCliente) {
+		$http.delete("fornecedor/deletar/" + codFornecedor).success(function(response) {
+			$scope.listarFornecedor($scope.numeroPagina);
+			sucesso("Removido com sucesso!");
+			
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+	};
+	
+    $scope.carregarCidades = function(estado) {
+		if (identific_nav() != 'chrome') {// executa se for diferente do chrome
+			$http.get("cidades/listar/" + estado.id).success(function(response) {
+				$scope.cidades = response;
+			}).error(function(data, status, headers, config) {
+				erro("Error: " + status);
+			});
+	  }
+	};
+	
+   $scope.carregarEstados = function() {
+		$scope.dataEstados = [{}];
+		$http.get("estados/listar").success(function(response) {
+			$scope.dataEstados = response;
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
+	
+});
+
+
+
+
+
 
 app.controller('clienteController', function($scope, $http, $location, $routeParams) {
 
