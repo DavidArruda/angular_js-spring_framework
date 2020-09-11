95aca43e380dcd2dc958fa1eb623441e93f8d262
David <deh0_@hotmail.com>
2020-09-10 21:21:58 -0300

    Criação do controller angularJs para livro

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -35,6 +35,25 @@ app.config(function($routeProvider) {
 				templateUrl : "fornecedor/cadastro.html"
 			})
 			
+			
+			
+			$routeProvider.when("/livrolist", {
+				controller : "livroController",
+				templateUrl : "livro/list.html"
+			})
+			
+			.when("/livroedit/:id", {
+				controller : "livroController",
+				templateUrl : "livro/cadastro.html"
+			})
+			
+			.when("/livro/cadastro", {
+				controller : "livroController",
+				templateUrl : "livro/cadastro.html"
+			})
+			
+			
+			
 			.otherwise({
 				redirectTo : "/"
 			});
@@ -266,6 +285,113 @@ app.controller('clienteController', function($scope, $http, $location, $routePar
 	};
 	
 });
+
+
+
+
+
+app.controller('livroController', function($scope, $http, $location, $routeParams) {
+
+	if($routeParams.id != null && $routeParams.id != undefined
+			&& $routeParams.id != ''){
+		
+	$http.get("livro/buscarlivro/" + $routeParams.id).success(function(response) {
+			$scope.livro = response;
+			
+			document.getElementById("imagemLivro").src = $scope.livro.foto;
+			
+				$http.get("fornecedor/listartodos/").success(function(response) {
+					$scope.fornecedoresList = response;
+					setTimeout(function () {
+						$("#selectFornecedores").prop('selectedIndex', buscarKeyJson(response, 'id', $scope.livro.fornecedor.id));
+					}, 1000);
+					
+				}).error(function(data, status, headers, config) {
+					erro("Error: " + status);
+				});
+			
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+		
+	}else {
+		$scope.livro = {};
+	}
+
+	$scope.editarLivro = function(id){
+		$location.path('livroedit/' + id);
+	};
+	
+	$scope.salvarLivro = function () {
+		$scope.livro.foto = document.getElementById("imagemLivro").getAttribute("src");
+	
+		$http.post("livro/salvar", $scope.livro).success(function(response) {
+			$scope.livro = {};
+			document.getElementById("imagemLivro").src = '';
+			sucesso("Gravado com sucesso!");
+			
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
+
+	$scope.listarLivros = function(numeroPagina){
+		$scope.numeroPagina = numeroPagina;
+		$http.get("livro/listar/" + numeroPagina).success(function(response) {
+			$scope.data = response;
+			
+			$http.get("livro/totalPagina").success(function(response) {
+				$scope.totalPagina = response;
+			}).error(function(response) {
+				erro("Error: " + response);
+			});
+
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
+
+	$scope.proximo = function(){
+		if(new Number($scope.numeroPagina) < new Number($scope.totalPagina)){
+			$scope.listarLivros(new Number($scope.numeroPagina + 1));
+		}
+		
+	};
+	
+	$scope.anterior = function(){
+		if(new Number($scope.numeroPagina) > 1) {
+			$scope.listarLivros(new Number($scope.numeroPagina - 1));
+		}
+	};
+
+	$scope.removerLivro = function(codLivro) {
+		$http.delete("livro/deletar/" + codLivro).success(function(response) {
+			$scope.listarLivros($scope.numeroPagina);
+			sucesso("Removido com sucesso!");
+			
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+	};
+	
+	$scope.listarFornecedores = function() {
+		$http.get("fornecedor/listartodos/").success(function(response) {
+			$scope.fornecedoresList = response;
+					
+		}).error(function(data, status, headers, config) {
+					erro("Error: " + status);
+		});
+	}
+	
+});
+
+
+
+
+
+
+
+
 
 function visualizarImg(){
 	var preview = document.querySelectorAll('img').item(1);
